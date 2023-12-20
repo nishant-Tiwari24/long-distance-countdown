2a74031c73c12d76e70579ad13d8022005dbd9cf
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-20 11:19:40 +0530

    🔗 enter date

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Timer from './components/Timer'
+import React, { useState } from 'react';
+import Timer from './components/Timer';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
+  const [targetDate, setTargetDate] = useState('');
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTargetDate(event.target.value);
+  };
 
   return (
-    <div className='main w-full h-screen fixed bg-zinc-800'>
-      <Navbar/>
-      <Timer/>
-    </div>
-  )
-}
 
-export default App
+    <div className='w-full h-screen fixed bg-zinc-700'>
+      <Navbar/>
+      <div className='flex gap-4 justify-end align-middle mt-5 mr-5 '>
+      <label className="text-red-200 text-1xl font-serif" htmlFor="targetDate">Enter Target Date: </label>
+      <input className="bg-red-200 rounded-[5px] pi-3 h-6 w-30 hover:border-red-600 border-red-300 border-[2px] border-solid" type="datetime-local" id="targetDate" placeholder="Select date" value={targetDate} onChange={handleDateChange} />
+      </div>
+      <Timer targetDate={targetDate} />
+
+    </div>
+  );
+};
+
+export default App;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,80 +1,72 @@
-// import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
-// const HeroContent = () => {
-
-//   const [timerDays, setTimerDays] = React.useState(0);
-//   const [timerHours, setTimerHours] = React.useState(0);
-//   const [timerMinutes, setTimerMinutes] = React.useState(0);
-//   const [timerSeconds, setTimerSeconds] = useState(0);
-  
-//   let interval = useRef();
-  
-//   const startTimer = () => {
-//     const countdownDate = new Date('Jan 12, 2024 00:00:00').getTime();
-  
-//     interval.current = setInterval(() => {
-//       const now = new Date().getTime();
-//       const distance = countdownDate - now;
-  
-//       const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//       const seconds = Math.floor((distance % (1000 * 60)/60))
-  
-//       if (distance <= 0) {
-//         clearInterval(interval.current);
-//       } else {
-//         setTimerDays(days);
-//         setTimerHours(hours);
-//         setTimerMinutes(minutes);
-//         setTimerSeconds(seconds);
-//       }
-//     }, 1000);
-//   };
-  
-//   useEffect(() => {
-//     startTimer();
-//     return () => {
-//       clearInterval(interval.current);
-//     };
-//   }, []);
-// }
-
-function Timer() {     
-  return (
-
-    
-    <section className="timer-container">
-        <section className="timer absolute top-1/3 left-1/2 -translate-x-[50%] -translate-y-[50%] grid gap-40 mt-24">
-            <div>
-                <span className="mdi di-calender-clock ">
-                    <h2 className="text-red-200 text-2xl font-serif">🩷 Countdown to a really special date, To meet someone really special 🩷</h2>
-                </span>
-            </div>
-            <div className="flex text-red-200 text-center align-middle translate-x-[12%] -translate-y-[50%] justify-center text-3xl border-sky-100 border-[1px] border-separate rounded-[30px] w-[35rem] h-[15rem] py-[4.5rem] gap-4 font-serif">
-                <section>
-                    <h2 className="text-6xl">85</h2 >
-                    <p><small className="text-[15px]">Days</small></p>
-                </section>
-                <span>:</span>
-                <section>
-                    <h2 className="text-6xl">85</h2 >
-                    <p><small className="text-[15px]">Hours</small></p>
-                </section>
-                <span>: </span>
-                <section>
-                    <h2 className="text-6xl">85</h2 >
-                    <p><small className="text-[15px]">Minutes</small></p>
-                </section>
-                <span>: </span>
-                <section>
-                    <h2 className="text-6xl">85</h2 >
-                    <p><small className="text-[15px]">Seconds</small></p>
-                </section>
-            </div>
-        </section>
-    </section>
-  )
+interface TimerProps {
+  targetDate: string; 
 }
 
-export default Timer
+const Timer: React.FC<TimerProps> = ({ targetDate }) => {
+  const calculateTimeLeft = () => {
+    const targetTime = new Date(targetDate).getTime();
+    const now = new Date().getTime();
+    const timeDifference = targetTime - now;
+
+    if (timeDifference > 0) {
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+      return { days, hours, minutes, seconds };
+    } else {
+      // Timer has reached zero
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [targetDate]);
+
+  return (
+    <section className="timer-container">
+      <section className="timer absolute top-1/3 left-1/2 -translate-x-[50%] -translate-y-[50%] grid gap-40 mt-24">
+        <div>
+          <span className="mdi di-calender-clock ">
+            <h2 className="text-red-200 text-2xl font-serif">🩷 Countdown to a really special date, To meet someone really special 🩷</h2>
+          </span>
+        </div>
+        <div className="flex text-red-200 text-center align-middle translate-x-[12%] -translate-y-[50%] justify-center text-3xl border-sky-100 border-[1px] border-separate rounded-[30px] w-[35rem] h-[15rem] py-[4.5rem] gap-4 font-serif">
+          <section>
+            <h2 className="text-6xl">{timeLeft.days}</h2>
+            <p><small className="text-[15px]">Days</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <h2 className="text-6xl">{timeLeft.hours}</h2>
+            <p><small className="text-[15px]">Hours</small></p>
+          </section>
+          <span>: </span>
+          <section>
+            <h2 className="text-6xl">{timeLeft.minutes}</h2>
+            <p><small className="text-[15px]">Minutes</small></p>
+          </section>
+          <span>: </span>
+          <section>
+            <h2 className="text-6xl">{timeLeft.seconds}</h2>
+            <p><small className="text-[15px]">Seconds</small></p>
+          </section>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Timer;
