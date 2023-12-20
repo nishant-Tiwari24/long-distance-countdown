b1129d4664ee57bb96a8a25333c7a95629254f15
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-20 11:36:31 +0530

    🔐 added date

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
       <Navbar/>
       <div className='flex gap-4 justify-end align-middle mt-5 mr-5 '>
       <label className="text-red-200 text-1xl font-serif" htmlFor="targetDate">Enter Target Date: </label>
-      <input className="bg-red-200 rounded-[5px] pi-3 h-6 w-30 hover:border-red-600 border-red-300 border-[2px] border-solid" type="datetime-local" id="targetDate" placeholder="Select date" value={targetDate} onChange={handleDateChange} />
+      <input className="bg-red-200 rounded-[4px] h-6 w-30 hover:border-red-600 :text-zinc-500 type:font-sans placeholder:font-semibold border-red-300 border-[2px] border-solid" placeholder='Select date' id="targetDate" type='date' value={targetDate} onChange={handleDateChange} />
       </div>
       <Timer targetDate={targetDate} />
 
