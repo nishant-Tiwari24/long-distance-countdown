65c9aeab575619c17ce66899fedabdf0fd6d47c6
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-20 22:54:29 +0530

    Update Timer.tsx

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -10,7 +10,7 @@ const Timer: React.FC<TimerProps> = ({ targetDate }) => {
     const now = new Date().getTime();
     const timeDifference = targetTime - now;
 
-    if (timeDifference > 0) {
+    if (timeDifference > 0 || timeDifference < 0) {
       const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
       const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
