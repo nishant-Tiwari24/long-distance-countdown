46b1affd59966536e3512c9f7032c4e020dee0ec
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-12-20 12:29:05 +0530

    falling css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Timer from './components/Timer';
 import Navbar from './components/Navbar';
+import Leaves from './components/Leaves';
 
 const App = () => {
   const [targetDate, setTargetDate] = useState('');
@@ -11,14 +12,14 @@ const App = () => {
 
   return (
 
-    <div className='w-full h-screen fixed bg-zinc-700'>
+    <div className='w-full h-screen fixed bg-[radial-gradient(#333,#000)]'>
       <Navbar/>
       <div className='flex gap-4 justify-end align-middle mt-5 mr-5 '>
       <label className="text-red-200 text-1xl font-serif" htmlFor="targetDate">Enter Target Date: </label>
       <input className="bg-red-200 rounded-[4px] h-6 w-30 hover:border-red-600 :text-zinc-500 type:font-sans placeholder:font-semibold border-red-300 border-[2px] border-solid" placeholder='Select date' id="targetDate" type='date' value={targetDate} onChange={handleDateChange} />
       </div>
       <Timer targetDate={targetDate} />
-
+      <Leaves/>
     </div>
   );
 };
---
+++ src/components/Leaves.tsx
@@ -0,0 +1,48 @@
+const Leaves = () => {
+  return (
+    <section className=" w-full h-screen ">
+    <div className="set">
+        <div><img src="src/assets/leaves1.png" alt="" /></div>
+        <div><img src="src/assets/leaves2.png" alt="" /></div>
+        <div><img src="src/assets/leaves3.png" alt="" /></div>
+        <div><img src="src/assets/leaves4.png" alt="" /></div>
+        <div><img src="src/assets/leaves1.png" alt="" /></div>
+        <div><img src="src/assets/leaves2.png" alt="" /></div>
+        <div><img src="src/assets/leaves3.png" alt="" /></div>
+        <div><img src="src/assets/leaves4.png" alt="" /></div>
+    </div>
+    <div className="set set2">
+        <div><img src="src/assets/leaves1.png" alt="" /></div>
+        <div><img src="src/assets/leaves2.png" alt="" /></div>
+        <div><img src="src/assets/leaves3.png" alt="" /></div>
+        <div><img src="src/assets/leaves4.png" alt="" /></div>
+        <div><img src="src/assets/leaves1.png" alt="" /></div>
+        <div><img src="src/assets/leaves2.png" alt="" /></div>
+        <div><img src="src/assets/leaves3.png" alt="" /></div>
+        <div><img src="src/assets/leaves4.png" alt="" /></div>
+    </div>
+    <div className="set set3">
+        <div><img src="src/assets/leaves1.png" alt="" /></div>
+        <div><img src="src/assets/leaves2.png" alt="" /></div>
+        <div><img src="src/assets/leaves3.png" alt="" /></div>
+        <div><img src="src/assets/leaves4.png" alt="" /></div>
+        <div><img src="src/assets/leaves1.png" alt="" /></div>
+        <div><img src="src/assets/leaves2.png" alt="" /></div>
+        <div><img src="src/assets/leaves3.png" alt="" /></div>
+        <div><img src="src/assets/leaves4.png" alt="" /></div>
+    </div>
+    <div className="set">
+        <div><img src="src/assets/leaves1.png" alt="" /></div>
+        <div><img src="src/assets/leaves2.png" alt="" /></div>
+        <div><img src="src/assets/leaves3.png" alt="" /></div>
+        <div><img src="src/assets/leaves4.png" alt="" /></div>
+        <div><img src="src/assets/leaves1.png" alt="" /></div>
+        <div><img src="src/assets/leaves2.png" alt="" /></div>
+        <div><img src="src/assets/leaves3.png" alt="" /></div>
+        <div><img src="src/assets/leaves4.png" alt="" /></div>
+    </div>
+    </section>
+  )
+}
+
+export default Leaves
